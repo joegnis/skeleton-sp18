c17b73657deb95da91f59d15b426bb1840de1192
joegnis <joegnis@pm.me>
2022-03-07 17:48:26 -0800

    [Lab11] Completes MazeBreadthFirstPaths

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,10 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -9,21 +12,48 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private final int source;
+    private final int target;
+    private final Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        source = maze.xyTo1D(sourceX, sourceY);
+        target = maze.xyTo1D(targetX, targetY);
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> fringe = new ArrayDeque<>();
+        fringe.offer(source);
+        distTo[source] = 0;
+        edgeTo[source] = source;
+        marked[source] = true;
+        announce();
+
+        while (!fringe.isEmpty()) {
+            int vertex = fringe.poll();
+            for (int neighbor : maze.adj(vertex)) {
+                if (marked[neighbor]) {
+                    continue;
+                }
+                fringe.offer(neighbor);
+                distTo[neighbor] = distTo[vertex] + 1;
+                edgeTo[neighbor] = vertex;
+                marked[neighbor] = true;
+                announce();
+                if (neighbor == target) {
+                    return;
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
